dfb9907c822add0c9fec46ec9b26aeffd5d23718
ÖZKAN TEKE <OZ_TEKE_93@hotmail.com>
2021-05-02 16:19:37 +0300

    4. day homework 1

---
+++ staticDemo/src/staticDemo/DatabaseHelper.java
@@ -0,0 +1,22 @@
+package staticDemo;
+
+public class DatabaseHelper {
+	
+	public static class Crud{ // create read update delete
+		public static void delete() {
+			
+		}
+		
+		public static void update() {
+			
+		}
+		
+	}
+	
+	public static class Connection { // inner class tavsiye edilmez
+		public static void createConnection() {
+			
+		}
+	}
+
+}
